465c9803991572ea5f8b32a8db2ef709751bd5f6
Ismail shaikh <shaikhismailinfo@gmail.com>
2026-09-10 21:31:11 +0530

    Initial Setup for nx workspace and dependencies

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -5,6 +5,17 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
+//Optimus UI configurations
+import { provideOptimus } from '@openng/optimus-ui/config';
+import Aura from '@openng/optimus-ui-themes/aura';
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(), 
+    provideRouter(appRoutes),
+    provideOptimus({
+      theme: {
+        preset: Aura
+      }
+    })
+  ],
 };
